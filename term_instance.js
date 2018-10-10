8c0ffada561c89b5506f60874c7ab46de2f4871b
cbrown-twiage <cbrown@twiagemed.com>
2018-10-10 12:15:15 -0400

    gets desired, min, and max capacity from ASG name input

--- term_instance.js
+++ term_instance.js
@@ -1,9 +1,20 @@
 const AWS = require("aws-sdk");
 
 const ec2 = new AWS.EC2();
+const asg = new AWS.AutoScaling();
 
 exports.handle = event => {
   // double ASG MaxSize, MinSize, and DesiredCapacity
+  asg.describeAutoScalingGroups(event.targetASG, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let desiredCapacity = data.AutoScalingGroups[0].DesiredCapacity * 2;
+      let max = data.AutoScalingGroups[0].MaxSize * 2;
+      let min = data.AutoScalingGroups[0].MinSize * 2;
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/AutoScaling.html#putScalingPolicy-property
+    }
+  });
   // when new instances are ready, return ASG MaxSize, MinSize, and DesiredCapacity back to original values
   // ASG should terminate old instances
 };
